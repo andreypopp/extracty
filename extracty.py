cb3a5edb7b6750d130f672221fe1f5ebf8df3739
Andrey Popp <8mayday@gmail.com>
2012-08-13 15:17:49 +0400

    initial commit

---
+++ extracty.py
@@ -0,0 +1,112 @@
+"""
+
+    extracty -- metadata extraction from HTML documents
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+
+__all__ = ('extract_author',)
+
+def extract_author(doc):
+    """ Extract author from ``doc``
+
+    :param doc:
+        HTML document as a string or as a parsed
+    """
+
+    if isinstance(doc, basestring):
+        doc = lxml.html.fromstring(doc)
+
+    def _find_meta(doc):
+        for name in ('author', 'blogger', 'creator', 'publisher'):
+            meta = doc.xpath('//meta[@name="%s"]' % name)
+            if not meta:
+                continue
+            meta = meta[0]
+            text = meta.attrib.get('content')
+            if not text:
+                continue
+            if re.search(r'\.[a-z]{2,4}$', text, re.I):
+                continue
+            return text
+
+    def _find_itemprop(doc):
+        es = doc.xpath('//*[@itemprop="author"]')
+        for e in es:
+            text = html_to_text(e)
+            if text:
+                return text
+
+    def _find_rel(doc):
+        es = doc.xpath('//*[@rel="author"]')
+        for e in es:
+            text = html_to_text(e)
+            if text:
+                return text
+
+    def _find_heueristics(doc):
+
+        seen = []
+
+        for e in doc.iter():
+
+            text = html_to_text(e)
+            found = False
+
+            if matches_attr(_comment_classes, e, 'class', 'id'):
+                continue
+
+            if len(text) > 80:
+                continue
+
+            if _author_content.search(text) or _author_content_2.search(text):
+                found = True
+
+            if (
+                matches_attr(_author_classes, e, 'class', 'id')
+                and not matches_attr(_author_classes_banned, e, 'class', 'id')):
+                if not text:
+                    continue
+                found = True
+
+            if found:
+                for (el, _) in seen[:]:
+                    if text in el:
+                        seen.remove((el, _))
+                seen.append((text, list(e.itertext())))
+
+        if seen:
+            return seen[0]
+
+    def _best_part(parts):
+        parts = parts[:]
+        for part in parts[:]:
+            if not part or not part.strip():
+                parts.remove(part)
+            elif re.search(r'[0-9]', part):
+                parts.remove(part)
+            elif not re.sub(r'[^a-z]+', '', part, flags=re.I):
+                parts.remove(part)
+        if parts:
+            return parts[0]
+
+    def _clean(author, parts):
+        if parts:
+            parts = [p.strip() for p in parts if p and p.strip() and len(p) > 1]
+            author = ' , '.join(parts)
+        author = re.sub(r'.*by($|\s)', '', author, flags=re.I)
+        author = re.sub(r'^[^a-z]+', '', author, flags=re.I)
+        splitter = re.compile(r'( at )|( on )|[,\|]', re.I)
+        if splitter.search(author):
+            parts = splitter.split(author)
+            author = _best_part(parts)
+        return author.strip() if author else None
+
+    for finder in (_find_itemprop, _find_meta, _find_heueristics, _find_rel):
+        maybe_author = finder(doc)
+        if maybe_author is not None:
+            if isinstance(maybe_author, tuple):
+                text, parts = maybe_author
+                return _clean(text, parts)
+            else:
+                return _clean(maybe_author, None)
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+    name='extracty',
+    version=version,
+    description='various utilities to extract metadata from HTML documents',
+    author='Andrey Popp',
+    author_email='8mayday@gmail.com',
+    license='BSD',
+    py_modules=['extracty'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'lxml',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """)
