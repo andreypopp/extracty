c56c1ec33820145725a94eae287c57d1730e3d2b
Andrey Popp <8mayday@gmail.com>
2012-08-16 20:10:47 +0400

    extracty.content: fix justext usage (do not strip unwanted els)

--- extracty/content.py
+++ extracty/content.py
@@ -26,17 +26,24 @@ def extract_content(doc, url, html=True):
     """
     if isinstance(doc, basestring):
         doc = lxml.html.fromstring(doc)
+
     remove_non_content(doc)
+    remove_bad_by_attrs(doc)
     remove_bad_by_classifier(doc)
-    rewrite_links(doc, url)
-    clean(doc, strip_attrs=True)
+
+    clean(doc, strip_attrs=False)
+
     remove_empty_elements(doc)
     doc = unwrap_elements(doc)
+
+    rewrite_links(doc, url)
+
     return lxml.html.tostring(doc, pretty_print=True)
 
 def remove_empty_elements(doc):
     to_delete = []
     for el in doc.iter():
+        # just left those have imgs
         if el.tag == 'img' or el.xpath('.//img'):
             continue
         if not html_to_text(el).strip():
@@ -56,21 +63,39 @@ def remove_bad_by_classifier(doc):
     ps = justext.justext(
         doc, justext.get_stoplist('English'))
     to_delete = []
+    good = []
     for p in ps:
         if p['class'] == 'bad':
             for el in doc.xpath(p['xpath']):
-                to_delete.append(el)
+                to_delete.append((el, p['xpath']))
+        elif p['class'] == 'good':
+            good.append(p['xpath'])
+
+    for el, xp in reversed(to_delete):
+        if el.getparent() is not None and not any(xp in g for g in good):
+            el.drop_tree()
+
+def remove_bad_by_attrs(doc):
+    to_delete = []
+    for el in doc.iter():
+        if (
+            matches_attr(_bad_attr_re, el, 'class', 'id')
+            and not any(
+                matches_attr(_good_attr_re, x, 'class', 'id')
+                for x in el.iter())):
+            to_delete.append(el)
+
     for el in reversed(to_delete):
         if el.getparent() is not None:
             el.drop_tree()
 
-def clean(doc, strip_attrs=True):
+def clean(doc, strip_elements=True, strip_attrs=True):
+    """ Clean ``doc`` DOM"""
     to_delete = []
     for el in doc.iter():
 
-        if matches_attr(_bad_attr_re, el, 'class', 'id'):
-            to_delete.append(el)
-
+        # stript unwanted attrs
+        # XXX: should we left height/width for img?
         if strip_attrs:
             for k in el.attrib.keys():
                 if (
@@ -78,18 +103,15 @@ def clean(doc, strip_attrs=True):
                     or k.startswith('data-')):
                     del el.attrib[k]
 
+        # strip text nodes
         if el.tail:
             el.tail = el.tail.strip()
         if el.text:
             el.text = el.text.strip()
 
-    for el in reversed(to_delete):
-        if el.getparent() is not None:
-            el.drop_tree()
-
 def unwrap_elements(doc):
 
-    if doc.tag == 'html':
+    if doc.tag == 'html' and len(doc) > 0:
         return unwrap_elements(doc[0])
     elif doc.tag in ('body', 'div'):
         if doc.text or doc.tail:
@@ -98,7 +120,6 @@ def unwrap_elements(doc):
             return unwrap_elements(doc[0])
         else:
             return lxml.builder.E.div(*doc)
-
 
     return doc
 
@@ -144,8 +165,23 @@ _bad_attr_re = gen_matches_any(
     'ad-',
     'sidebar',
     'tweet',
-    'subscrib',
-    'subscript',
+    'subscri',
     'buy',
+    'header',
     '(^|\-|_)date($|\-|_)',
+    )
+
+_good_attr_re = gen_matches_any(
+    'article',
+    'body',
+    'content',
+    'entry',
+    'hentry',
+    'main',
+    'page',
+    'pagination',
+    'post',
+    'text',
+    'blog',
+    'story',
     )
