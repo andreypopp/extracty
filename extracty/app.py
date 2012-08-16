a7deb881a12d83032e17bfc536dc12654bdd634f
Andrey Popp <8mayday@gmail.com>
2012-08-16 15:28:08 +0400

    extracty.app: basic html template

--- extracty/app.py
+++ extracty/app.py
@@ -18,22 +18,52 @@ __all__ = ('application',)
 
 def application(environ, start_response):
     """ WSGI application"""
-    def response(data, status="200 Success", headers=[]):
+
+    is_view = environ['PATH_INFO'] == '/view'
+
+    def response(data, status="200 Success"):
+        if is_view:
+            headers = [('Content-type', 'text/html')]
+        else:
+            headers = [('Content-type', 'application/json')]
         start_response(str(status), headers)
-        return [json.dumps(data)]
+        return [json.dumps(data)] if not is_view else data
 
     def error(message):
-        return response({"error": message}, status="400 Error")
+        msg = {"error": message} if not is_view else message
+        return response(msg, status="400 Error")
 
-    qs = urlparse.parse_qs(environ['QUERY_STRING'])
-    if not 'url' in qs:
-        return error("missing 'url' parameter")
-    kwargs = {}
-    for kw in ('cover_image', 'author', 'title'):
-        key = 'no_%s' % kw
-        if key in qs:
-            kwargs[kw] = False
-    url = qs['url']
-    doc = fetch_url(url)
-    result = extract(doc, url, **kwargs)
-    return response(result)
+    def get_result():
+        qs = urlparse.parse_qs(environ['QUERY_STRING'])
+        if not 'url' in qs:
+            return error("missing 'url' parameter")
+        kwargs = {}
+        for kw in ('cover_image', 'author', 'title'):
+            key = 'no_%s' % kw
+            if key in qs:
+                kwargs[kw] = False
+        url = qs['url'][0]
+        doc = fetch_url(url)
+        return extract(doc, url, **kwargs)
+
+    result = get_result()
+    import ipdb
+    ipdb.set_trace()
+    return response(result) if not is_view else response(template % result)
+
+template = """
+<!doctype html>
+<style>
+    .author, .url {
+        font-size: %%80;
+        color: #666;
+    }
+</style>
+<div>
+    <div class="author">%(author)s</div>
+    <div class="url">%(url)s</div>
+    <img src="%(cover_image)s">
+    <h1 class="title">%(title)s</h1>
+    <div class="content">%(content)s</div>
+</div>
+"""
